d03a667ff5e2f5a262c6c8d68e7588c32717bbb7
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-15 18:11:43 +0000

    Create unverify.js

---
+++ commands/unverify.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args, func) => {
+	let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!member)
+  return message.reply("Please mention a valid member of this server");
+  if (!message.member.roles.some(r => ["Dyno Verification"].includes(r.name)))
+      return message.channel.send("Sorry, you don't have permissions to use this!");
+	let role = message.guild.roles.find(r => r.name === "Verified");
+	let role2 = message.guild.roles.find(r => r.name === "=== LR Section ===");
+	let role3 = message.guild.roles.find(r => r.name === "Unverified");
+
+	member.addRole(role3).catch(console.error);
+	member.removeRole(role).catch(console.error);
+	member.removeRole(role2).catch(console.error);
+  message.channel.send(`<:success:523531815072301092> Changed roles for ${member.user.tag}, +${role2.name}, -${role.name}, -${role2.name}`)
+  client.guilds.get("479268202866540564").channels.get("517118784016744448").send(`${message.author} used **unverify**`)
+}
