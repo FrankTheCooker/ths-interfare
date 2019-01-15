840afd56462e6b7b57daa8219e07baa4f67f63d2
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-15 20:20:59 +0000

    Update and rename confirmtest to confirmtest.js

--- commands/confirmtest.js
+++ commands/confirmtest.js
@@ -3,7 +3,4 @@ const Discord = require('discord.js');
 exports.run = (client, message, args, func) => {
 
 message.reply("Oof! This command is in progress, so wait until this command is actually made for no--- **How did you find out about this command?!!**")
-
-
-
 }
