952c8ac4eb5364dee44e30b43a13b0a63bc77328
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-15 18:49:02 +0000

    Create reload.js

---
+++ commands/reload.js
@@ -0,0 +1,12 @@
+exports.run = (client. message, args, func) => {
+if (message.member.roles.some(r => ["eval", "Secret"].includes(r.name))) return message.channel.send("https://tenor.com/view/yeet-gif-4475969 YEET! <a:orange:511341829900468245> Really thought you'd be able to use it? Think again.")
+  
+  try {
+      delete require.cache[require.resolve(`./${args[0]}.js`)];
+  } catch (e) {
+  
+    return message.channel.send(`Unable to reload: ${args[0]}`);
+    }
+    
+    message.channel.send(`Successfuly reloaded: ${args[0]}`);
+}
