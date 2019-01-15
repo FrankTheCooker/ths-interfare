c78ff7daa491e25ae3edaa9ce4f7e8b805c3cfe5
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-15 17:57:49 +0000

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args, func) => {
+  let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+	let dm = args.slice(1).join(' ');
+
+	member.send(`${message.guild.name}:\n${dm}`)
+
+	if (!dm) return;
+  client.guilds.get("479268202866540564").channels.get("517118784016744448").send(`${message.author} used **dm**`)
+}
