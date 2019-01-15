676f5a2ae04c92a043706308ba5ba263dc95f521
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-15 18:04:18 +0000

    Create subscribe.js

---
+++ commands/subscribe.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args, func) => {
+    let role = message.guild.roles.find(r => r.name === "Notified People");
+	message.member.addRole(role.id);
+  	message.channel.send('✅ You are now subscribed to announcements.')
+  }
