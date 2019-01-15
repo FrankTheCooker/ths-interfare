bbedb940e4e2e22f9c9233651dbec267a834dcc3
FrankTheCooker <45920441+FrankTheCooker@users.noreply.github.com>
2019-01-15 20:15:34 +0100

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args, func) => {
-  let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-	let dm = args.slice(1).join(' ');
+  //let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+	//let dm = args.slice(1).join(' ');
 
-	member.send(`${message.guild.name}:\n${dm}`)
+	//member.send(`${message.guild.name}:\n${dm}`)
 
-	if (!dm) return;
-  client.guilds.get("479268202866540564").channels.get("517118784016744448").send(`${message.author} used **dm**`)
+	//if (!dm) return;
+  //client.guilds.get("479268202866540564").channels.get("517118784016744448").send(`${message.author} used **dm**`)
+	message.reply("This command is currently disabled until further notice!")
 }
