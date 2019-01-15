ebea9ccc2b1074e35161601fadbf8b0596867c03
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-15 18:13:09 +0000

    Create working.js

---
+++ commands/working.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args, func) => {
+  if (!message.member.roles.some(r => ["+"].includes(r.name)))
+      return message.channel.send('You can\'t change that.')
+    client.guilds.get("479268202866540564").channels.get("528939321097715722").send('Changed bot status to **Operative**.')
+    message.channel.send('Good, I have changed it to `operative`.')
+    client.guilds.get("479268202866540564").channels.get("517118784016744448").send(`${message.author} has put the bot to **Operative**.`)
+}
