f77b12d90525337e0f14e8d4aab909b82a199433
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-16 18:37:41 +0000

    Create events.js

---
+++ commands/events.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args, func) => {
+message.channel.send("Welcome to the new event tab!\n\nThese are the upcoming events!\nThere are currently no events planned, it will be notified whenever it gets updated, mostly weekly.\n\n\n__**Want to host an event?**__\nPlease DM Frankie#4073 for more info!")
+}
