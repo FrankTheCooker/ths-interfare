570f2f9f2f5088072de612d4385a81fcab9de46d
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-16 20:14:51 +0000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,5 +2,5 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args, func) => {
 	let target = message.mentions.users.first() || message.guild.member(args[0]);
-        let msg = await message.channel.send(`\`${target.user.tag}\`'s avatar is: ` + target.dsiplayAvatarURL);
+        message.channel.send(`\`${target.user.tag}\`'s avatar is: ` + target.displayAvatarURL);
 }
