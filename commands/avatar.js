416f3191a650e7eb0eb7da38d2959bf1a935bd0d
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-16 18:52:35 +0000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,5 +3,5 @@ const sm = require('string-similarity');
 
 exports.run = (client, message, args, func) => {
   let member = args[0] || message.mentions.users.first || message.author
-  message.channel.send(`\`${member.tag}\`'s avatar is: ${member.user.displayAvatarURL({size: 1024})}`)
+  message.channel.send(`\`${member.tag}\`'s avatar is: ${member.user.displayAvatarURL}`)
 }
