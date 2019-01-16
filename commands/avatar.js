0abe505de6928679eab9a51b665c421646ee4960
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-16 18:49:52 +0000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,27 +2,6 @@ const Discord = require('discord.js');
 const sm = require('string-similarity');
 
 exports.run = (client, message, args, func) => {
-  
-  let members = [];
-  let indexes = [];
-  
-  message.guild.members.forEach(function(member){
-    members.push(member.user.username);
-    indexes.push(member.id);
-  })
-  let match = sm.findBestMatch(args.join(' '), members);
-  let username = match.bestMatch.target;
-  let member = message.guild.members.get(indexes[members.indexOf(username)])
-  let usernameUser = '';
-  let UserMention = '';
-  
-  if(!args[0]) {
-    usernameUser = message.author
-    UserMention = message.member
-  } else {
-  let mention = message.mentions.users.first()
-    usernameUser = mention || member.user
-    UserMention = message.mentions.members.first() || message.guild.members.get(args[0]) || member
-  }
-  message.channel.send(`\`${usernameUser.tag}\`'s avatar is: ${usernameUser.displayAvatarURL({size: 1024})}`)
+  let member = args[0] || message.mentions.users.first || message.author
+  message.channel.send(`\`${member.tag}\`'s avatar is: ${member.displayAvatarURL({size: 1024})}`)
 }
