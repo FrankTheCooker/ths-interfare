305f4deb3286247d385da1939e27e577c6a7237b
FrankTheCooker <45920441+FrankTheCooker@users.noreply.github.com>
2019-01-19 17:03:55 +0100

    Update newcommands.js

--- commands/newcommands.js
+++ commands/newcommands.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args, func) => {
     
     const embed = new Discord.RichEmbed()
   .setTitle("New commands")
-  .setDescription("Showing new commands.\n\n\n**New normal/fun commands**\nlsar - Shows colour list.\nping - Gives the bot ping.\nrps - Rock Paper Scissors.\n\n**New staff commands**\nNone.\n\n**New executive commands**\nlock - Lock the bot.\nunlock - Unlocks the bot\nworking - Put the bot to `operative`.\nbroken - Put the bot to `non-operative.")
+  .setDescription("Showing new commands.\n\n\n**New normal/fun commands**\nlsar - Shows colour list.\nserverinvite - Gives perm server invite\n\n**New staff commands**\nNone.\n\n**New executive commands**\nNone.")
   .setColor (0x3300CC)
     
   message.channel.send(embed)
