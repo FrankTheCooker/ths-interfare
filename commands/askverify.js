4d68f7afe3eb1696e9bd900fb0718da0b525b40f
FrankTheCooker <45920441+FrankTheCooker@users.noreply.github.com>
2019-01-19 17:27:46 +0100

    Update askverify.js

--- commands/askverify.js
+++ commands/askverify.js
@@ -1,8 +1,15 @@
 const Discord = require('discord.js');
+const talkedRecently = new Set();
 
 exports.run = (client, message, args, func) => {
-  let mentionrole = message.guild.roles.find(r => r.name === "Dyno Verification");
+	if (talkedRecently.has(message.author.id))
+		talkedRececntly.add(message.author.id);
+	talkedRecently.delete(message.author.id);
+
+ let mentionrole = message.guild.roles.find(r => r.name === "Dyno Verification");
+	if (!message.member.roles.some(r => ["Unverified"].includes(r.name)))
+return message.channel.send("Sorry, you have already been verified!");
   
   client.guilds.get("479268202866540564").channels.get("498163437562495006").send(`Hey ${message.author}, you have requested verify!`)
 	client.guilds.get("479268202866540564").channels.get("517118784016744448").send(`Hey, ${mentionrole}! ${message.author} has requested verify!`)
-}
+}, 2500);
