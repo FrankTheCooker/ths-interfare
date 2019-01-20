ae1801fa70ce68e1ef9ab394eed47e9d17f40592
FrankTheCooker <45920441+FrankTheCooker@users.noreply.github.com>
2019-01-20 17:52:48 +0100

    Update events.js

--- commands/events.js
+++ commands/events.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args, func) => {
-message.channel.send("Welcome to the new event tab!\n\nThese are the upcoming events!\nHunger Games: Hosted by \`MaC#8311\`.\n\n__**Want to host an event?**__\nPlease DM Frankie#4073 for more info!")
+message.channel.send("Welcome to the new event tab!\n\nThese are the upcoming events!\nHunger Games: Hosted by \`MaC#8311\`.\nAn week with Frank! Expierence a week what Frank does, DM Frankie#4073 for the events role for this event (THIS WILL BE FOR WEEK 5)! Hosted by: Frank.\n\n__**Want to host an event?**__\nPlease DM Frankie#4073 for more info!")
 }
