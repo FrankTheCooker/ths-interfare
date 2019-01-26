8e9c07a5fd1a7db13d9d1fca6877a3bbc6e59baf
macisongithub <44820315+macisongithub@users.noreply.github.com>
2019-01-26 19:02:56 +0000

    Update events.js

--- commands/events.js
+++ commands/events.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args, func) => {
 // message.channel.send("Welcome to the new event tab!\n\nThese are the upcoming events!\nHunger Games: Hosted by \`MaC#8311\`.")
 const embed = new Discord.RichEmbed()
-  .setColor(0x7289DA)
+.setColor(0x7289DA)
 .setTitle("THS Interfare Events List")
-.addField("Perm Events", `Hunger Games, Hosted by: MaC#8311\nParticipate the event by going to <#535178568490156063>`)
-.addField("**Want to host an event? DM __Frankie#4073__ for more information!**")
+.addField("Perm Events", `Hunger Games, Hosted by \`MaC#8311\`.\nYou can participate by going to <#535178568490156063>.\nNote: Held every Saturday unless cancelled or delayed (early notice).`)
+.addField("Want to host an event?", "DM `\Frankie#4073\` for more information!")
 
 message.channel.send(embed) 
 }
