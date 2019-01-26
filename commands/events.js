396a73739e30bf39e485aacf7876994cfeeea202
FrankTheCooker <45920441+FrankTheCooker@users.noreply.github.com>
2019-01-26 19:46:22 +0100

    Update events.js

--- commands/events.js
+++ commands/events.js
@@ -1,3 +1,9 @@
 exports.run = (client, message, args, func) => {
-message.channel.send("Welcome to the new event tab!\n\nThese are the upcoming events!\nHunger Games: Hosted by \`MaC#8311\`.\nAn week with Frank! Expierence a week what Frank does, DM Frankie#4073 for the events role for this event (THIS WILL BE FOR WEEK 5)! Hosted by: Frank.\n\n__**Want to host an event?**__\nPlease DM Frankie#4073 for more info!")
+// message.channel.send("Welcome to the new event tab!\n\nThese are the upcoming events!\nHunger Games: Hosted by \`MaC#8311\`.")
+const embed = new Discord.RichEmbed()
+  .setColor(0x7289DA)
+.setTitle("THS Interfare Events List")
+.addField("Perm Events", `Hunger Games, Hosted by: MaC#8311\nParticipate the event by going to <#535178568490156063>`)
+.addField("**Want to host an event? DM __Frankie#4073__ for more information!**)
 }
+          
